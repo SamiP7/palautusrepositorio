6b0f92e61b4d373e0ee9924e435ecedbd9112955
samip <sami.parkkinenn@gmail.com>
2022-11-23 07:04:24 +0200

    kahden saman tuotteen lisayksen jalkeen on korin hinta tuotteiden hinta

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -46,3 +46,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_saman_tuotteen_lisayksen_jalkeen_on_korin_hinta_tuotteiden_hinta(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+
+        self.assertEqual(self.kori.hinta(), 6)
